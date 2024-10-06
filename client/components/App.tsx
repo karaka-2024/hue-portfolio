5b43a9d1258f84812aa68e128515e62192f1a7ef
Hue Nguyen <hue7347@gmail.com>
2024-10-06 14:07:23 +1300

    added css

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,8 +1,11 @@
+import Header from 'header.ts
+'
+
 function App() {
   return (
     <div>
       <h1>App</h1>
-      <p>React development has begun!</p>
+      <Header />
     </div>
   )
 }
---
+++ client/components/Header.tsx
@@ -0,0 +1,11 @@
+export function Header() {
+    return (
+    <>
+      <div className= "header">
+          <h1>Hue Nguyen Portfolio</h1>
+          <h3>Web Developer, Business Advisor, Project Manager</h3>
+      </div>
+      </>
+    )
+
+}
